3afad2d971e61080f1c224427050f9ccdb9623bb
Andrii Skliar <andreas.skliar@gmail.com>
2024-05-15 23:23:05 +0300

    update getAllEvents

--- controllers/events.js
+++ controllers/events.js
@@ -2,8 +2,11 @@ const { Event } = require("../models/events");
 
 const { ctrlWrapper } = require("../helpers");
 
-const getAllEvents = async (_, res) => {
-  const result = await Event.find({}, "-createdAt, -updatedAt");
+const getAllEvents = async (req, res) => {
+  const { page = 1, limit = 12 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const result = await Event.find({}, "-createdAt -updatedAt", { skip, limit });
   res.status(200).json(result);
 };
 
