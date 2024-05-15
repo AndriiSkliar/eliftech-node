df9da1e15fd25af842baa5a4973138ce188079ed
andreas.skliar@gmail.com <Andrii Skliar>
2024-05-15 11:09:11 +0300

    add schema by contact

--- models/contact.js
+++ models/contact.js
@@ -5,17 +5,25 @@ const contactSchema = new Schema(
   {
     name: {
       type: String,
-      required: [true, "Set name for contact"],
+      required: [true, "Set full name for contact"],
     },
     email: {
       type: String,
+      required: [true, "Set email for contact"],
     },
-    phone: {
+    dateOfBirth: {
       type: String,
+      required: [true, "Set date of birth for contact"],
     },
-    favorite: {
-      type: Boolean,
-      default: false,
+    event: {
+      type: String,
+      required: [true, "Change event for contact"],
+    },
+    eventFeedback: {
+      type: String, 
+      enum: ["Social media", "Friends", "Found myself"],
+      required: [true, "Set feedback for contact"],
+      default: "Social media",
     },
   },
   { versionKey: false, timestamps: true }
@@ -26,17 +34,13 @@ const Contact = model("contact", contactSchema);
 const addSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
-  phone: Joi.string().required(),
-  favorite: Joi.boolean(),
-});
-
-const updateFavoriteSchema = Joi.object({
-  favorite: Joi.boolean().required(),
+  dateOfBirth: Joi.string().required(),
+  event: Joi.string().required(),
+  eventFeedback: Joi.string().required(),
 });
 
 const schemas = {
   addSchema,
-  updateFavoriteSchema,
 };
 
 module.exports = {
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -16,13 +16,13 @@ router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
 router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateById);
 
-router.patch(
-  "/:id/favorite",
-  isValidId,
-  validateBody(schemas.updateFavoriteSchema),
-  ctrl.updateFavorite
-);
+// router.patch(
+//   "/:id/favorite",
+//   isValidId,
+//   validateBody(schemas.updateFavoriteSchema),
+//   ctrl.updateFavorite
+// );
 
-router.delete("/:id", isValidId, ctrl.deleteById);
+// router.delete("/:id", isValidId, ctrl.deleteById);
 
 module.exports = router;
