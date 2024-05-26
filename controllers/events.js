5fd94f7503e04fb4feedf4dab853037318cb9ab0
Andrii Skliar <andreas.skliar@gmail.com>
2024-05-26 15:46:04 +0300

    add sort

--- controllers/events.js
+++ controllers/events.js
@@ -3,10 +3,15 @@ const { Event } = require("../models/events");
 const { ctrlWrapper } = require("../helpers");
 
 const getAllEvents = async (req, res) => {
-  const { page = 1, limit = 12 } = req.query;
+  const { page = 1, limit = 12, sort = "" } = req.query;
   const skip = (page - 1) * limit;
+  const sortOption = sort ? { [sort]: 1 } : {};
 
-  const result = await Event.find({}, "-createdAt -updatedAt", { skip, limit });
+  const result = await Event.find({}, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).sort(sortOption);
+
   res.status(200).json(result);
 };
 
